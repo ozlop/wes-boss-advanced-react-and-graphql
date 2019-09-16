d14d052695ac51959902e31f417d0179e5a3934c
ozlop <oswaldo.t.lopez@gmail.com>
2019-09-16 16:24:01 -0700

    Add Header and Nav components

---
+++ sick-fits/frontend/components/Header.js
@@ -0,0 +1,15 @@
+import Nav from "./Nav";
+
+const Header = () => (
+  <div>
+    <div className="bar">
+      <a href="">Sickfits</a>
+    </div>
+    <Nav />
+    <div className="sub-bar">
+      <p>Search</p>
+    </div>
+  </div>
+);
+
+export default Header;
