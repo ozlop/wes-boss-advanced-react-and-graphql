387d89ff7116affc03bb3c28574180dfeb2823b3
ozlop <oswaldo.t.lopez@gmail.com>
2019-09-26 15:22:06 -0700

    Fix header styling typo

--- sick-fits/frontend/components/Header.js
+++ sick-fits/frontend/components/Header.js
@@ -22,7 +22,7 @@ const Logo = styled.h1`
   transform: skew(-7deg);
   a {
     padding: 0.5rem 1rem;
-    background: ${props => props.theme.red}
+    background: ${props => props.theme.red};
     color: white;
     text-transform: uppercase;
     text-decoration: none;
