bee20d785181cb9580143caa5d77e1b63ed2cf7a
ozlop <oswaldo.t.lopez@gmail.com>
2019-09-30 16:20:43 -0700

    Add GraphQL Yoga server configuration
    Created a prisma binding to the Prisma dev server created prior and used
    it to create the GraphQL Yoga server.
    
    The GraphQL Yoga server references the Mutations and Queries generated
    by the Prisma client.

--- sick-fits/backend/src/index.js
+++ sick-fits/backend/src/index.js
@@ -1,1 +1,20 @@
-// let's go!
+require("dotenv").config({ path: "variables.env" });
+const createServer = require("./createServer");
+const db = require("./db");
+
+const server = createServer();
+
+// TODO User express middle ware to handle cookies (JWT)
+// TODO User express middle ware to populate current user
+
+server.start(
+  {
+    cors: {
+      credentials: true,
+      origin: process.env.FRONTEND_URL
+    }
+  },
+  deets => {
+    console.log(`Server is now running on http://localhost:${deets.port}`);
+  }
+);
