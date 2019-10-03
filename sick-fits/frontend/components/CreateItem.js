50b7c06b4467e72cb5c124e9dea09fcef9b78b40
ozlop <oswaldo.t.lopez@gmail.com>
2019-10-03 16:27:58 -0700

    Add CreateItem component
    The CreateItem component has its own state that is updated using the
    components Form inputs. The state holds all input in the form fields.
    
    This state will be sent to a GraphQL Mutation in order to add the new
    Item to the database.

---
+++ sick-fits/frontend/components/CreateItem.js
@@ -0,0 +1,72 @@
+import React, { Component } from "react";
+import { Mutation } from "react-apollo";
+import gql from "graphql-tag";
+import Form from "./styles/Form";
+import formatMoney from "../lib/formatMoney";
+
+class CreateItem extends Component {
+  state = {
+    title: "",
+    description: "",
+    image: "",
+    largeImage: "",
+    price: 0
+  };
+  handleChange = e => {
+    const { name, type, value } = e.target;
+    const val = type === "number" ? parseFloat(value) : value;
+    this.setState({ [name]: val });
+  };
+
+  render() {
+    return (
+      <Form
+        onSubmit={e => {
+          e.preventDefault();
+        }}
+      >
+        <fieldset>
+          <label htmlFor="title">
+            Title
+            <input
+              type="text"
+              id="title"
+              name="title"
+              placeholder="Title"
+              required
+              value={this.state.title}
+              onChange={this.handleChange}
+            />
+          </label>
+          <label htmlFor="price">
+            Title
+            <input
+              type="number"
+              id="price"
+              name="price"
+              placeholder="Price"
+              required
+              value={this.state.price}
+              onChange={this.handleChange}
+            />
+          </label>
+          <label htmlFor="description">
+            Description
+            <textarea
+              id="description"
+              name="description"
+              placeholder="Description"
+              required
+              value={this.state.description}
+              onChange={this.handleChange}
+            />
+          </label>
+          <button type="submit">Submit</button>
+        </fieldset>
+        <h2>Sell an Item.</h2>
+      </Form>
+    );
+  }
+}
+
+export default CreateItem;
