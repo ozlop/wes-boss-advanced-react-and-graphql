d020a3555983c6045f83148e6fa05088e3f4fb0c
ozlop <oswaldo.t.lopez@gmail.com>
2019-10-25 16:39:44 -0700

    Add DeleteItem component
    Created a DeleteItem component to handle the deletion of shop items. The
    DeleteItem component uses a new deleteItem mutation to modify the
    database and updates the Apollo client by retrieving all items using the
    existing ALL_ITEMS query and filters out the deleted item. Updating the
    Apollo client cache directly allows for the deletion to be reflected
    instantaneously in the application, there is no subsequent database
    query needed to display items post-deletion.

---
+++ sick-fits/frontend/components/UpdateItem.js
@@ -0,0 +1,116 @@
+import React, { Component } from "react";
+import { Mutation, Query } from "react-apollo";
+import gql from "graphql-tag";
+import Router from "next/router";
+import Form from "./styles/Form";
+import formatMoney from "../lib/formatMoney";
+import Error from "./ErrorMessage";
+
+const SINGLE_ITEM_QUERY = gql`
+  query SINGLE_ITEM_QUERY($id: ID!) {
+    item(where: { id: $id }) {
+      id
+      title
+      description
+      price
+    }
+  }
+`;
+const UPDATE_ITEM_MUTATION = gql`
+  mutation UPDATE_ITEM_MUTATION(
+    $title: String!
+    $description: String!
+    $price: Int!
+  ) {
+    createItem(title: $title, description: $description, price: $price) {
+      id
+      title
+      description
+    }
+  }
+`;
+
+class UpdateItem extends Component {
+  state = {
+    title: "",
+    description: "",
+    image: "",
+    largeImage: "",
+    price: 0
+  };
+  handleChange = e => {
+    const { name, type, value } = e.target;
+    const val = type === "number" ? parseFloat(value) : value;
+    this.setState({ [name]: val });
+  };
+  updateItem = (e, updateItemMutation) => {
+    e.preventDefault();
+  };
+
+  render() {
+    return (
+      <Query
+        query={SINGLE_ITEM_QUERY}
+        variables={{
+          id: this.props.id
+        }}
+      >
+        {({ data, loading }) => {
+          if (loading) return <p>Loading...</p>;
+          if (!data.item) return <p>No Item Found for ID</p>;
+          return (
+            <Mutation mutation={UPDATE_ITEM_MUTATION} variables={this.state}>
+              {(updateItem, { loading, error }) => (
+                <Form onSubmit={e => this.updateItem(e, updateItem)}>
+                  <Error error={error} />
+                  <fieldset disabled={loading} aria-busy={loading}>
+                    <label htmlFor="title">
+                      Title
+                      <input
+                        type="text"
+                        id="title"
+                        name="title"
+                        placeholder="Title"
+                        required
+                        defaultValue={data.item.title}
+                        onChange={this.handleChange}
+                      />
+                    </label>
+                    <label htmlFor="price">
+                      Title
+                      <input
+                        type="number"
+                        id="price"
+                        name="price"
+                        placeholder="Price"
+                        required
+                        defaultValue={data.item.price}
+                        onChange={this.handleChange}
+                      />
+                    </label>
+                    <label htmlFor="description">
+                      Description
+                      <textarea
+                        id="description"
+                        name="description"
+                        placeholder="Description"
+                        required
+                        defaultValue={data.item.description}
+                        onChange={this.handleChange}
+                      />
+                    </label>
+                    <button type="submit">Submit</button>
+                  </fieldset>
+                  <h2>Sell an Item.</h2>
+                </Form>
+              )}
+            </Mutation>
+          );
+        }}
+      </Query>
+    );
+  }
+}
+
+export default UpdateItem;
+export { UPDATE_ITEM_MUTATION };
