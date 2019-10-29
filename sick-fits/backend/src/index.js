2ce2c1f3abd3db019bba62dafa2115b29ce8bccc
ozlop <oswaldo.t.lopez@gmail.com>
2019-10-29 16:50:35 -0700

    Add JWT token to cookies and use for user state
    Added JWT middleware to backend end app to retrieve user token from
    cookies and retrieve the userId. UserId information was added to the
    express req object in order to carry userId information across requests.

--- sick-fits/backend/src/index.js
+++ sick-fits/backend/src/index.js
@@ -1,4 +1,5 @@
 const cookieParser = require("cookie-parser");
+const jwt = require("jsonwebtoken");
 
 require("dotenv").config({ path: "variables.env" });
 const createServer = require("./createServer");
@@ -6,9 +7,18 @@ const db = require("./db");
 
 const server = createServer();
 
-// TODO User express middle ware to handle cookies (JWT)
 server.express.use(cookieParser());
-// TODO User express middle ware to populate current user
+
+// decode the JWT so we can get the user id on each request
+server.express.use((req, res, next) => {
+  const { token } = req.cookies;
+  if (token) {
+    const { userId } = jwt.verify(token, process.env.APP_SECRET);
+    // put the userId into the req for future requests to access
+    req.userId = userId;
+  }
+  next();
+});
 
 server.start(
   {
