32c137a5b207e7c26250a55da93ec8a4b21b7ffe
ozlop <oswaldo.t.lopez@gmail.com>
2019-10-29 15:53:38 -0700

    Add signup page and Signup component
    The sign up page will render three SignUp components to allow for the
    user to register, sign on, and delete user.
    
    The SignUp component consists of a form with its own internally managed
    state corresponding to each of the available fields.

---
+++ sick-fits/frontend/components/Signup.js
@@ -0,0 +1,59 @@
+import React, { Component } from "react";
+import { Mutation } from "react-apollo";
+import gql from "graphql-tag";
+import Form from "./styles/Form";
+import Error from "./ErrorMessage";
+
+class Signup extends Component {
+  state = {
+    name: "",
+    password: "",
+    email: ""
+  };
+  saveToState = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
+  render() {
+    return (
+      <Form>
+        <fieldset>
+          <h2>Signup</h2>
+          <label htmlFor="email">
+            Email
+            <input
+              type="email"
+              name="email"
+              placeholder="email"
+              value={this.state.email}
+              onChange={this.saveToState}
+            />
+          </label>
+          <label htmlFor="name">
+            Name
+            <input
+              type="text"
+              name="name"
+              placeholder="name"
+              value={this.state.name}
+              onChange={this.saveToState}
+            />
+          </label>
+          <label htmlFor="password">
+            Password
+            <input
+              type="password"
+              name="password"
+              placeholder="password"
+              value={this.state.password}
+              onChange={this.saveToState}
+            />
+          </label>
+          <button type="submit">Sign Up!</button>
+        </fieldset>
+      </Form>
+    );
+  }
+}
+
+export default Signup;
