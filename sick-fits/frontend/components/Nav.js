cdfbd2da24cd5ff99cab10a29db1ee7908539900
ozlop <oswaldo.t.lopez@gmail.com>
2019-10-30 14:44:33 -0700

    Fix user details rendering in Nav component

--- sick-fits/frontend/components/Nav.js
+++ sick-fits/frontend/components/Nav.js
@@ -5,8 +5,9 @@ import User from "./User";
 const Nav = () => (
   <NavStyles>
     <User>
-      {data => {
-        return <p>User</p>;
+      {({ data: { me } }) => {
+        if (me) return <p>{me.name}</p>;
+        return null;
       }}
     </User>
     <Link href="/items">
